60c1fe5cc67d2dfcaed4877202cc01e74204a606
Ankit Sharma <asuppal0478@gmail.com>
2024-05-19 21:57:36 +0530

    Done with product APIs as well

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import cors from 'cors'
 import authRoutes from "./routes/authRoute.js"
 
 import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 //const express = require ("express")
 
 //configure env
@@ -24,7 +25,8 @@ app.use(morgan('dev'));
 
 //routes
 app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/category",categoryRoutes) 
+app.use("/api/v1/category",categoryRoutes);
+app.use("/api/v1/product",productRoutes);
 // rest api
 app.get("/",(req,res)=>{
     res.send("<h1> Welcome to Sharma's world</h1>"
